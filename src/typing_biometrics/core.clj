d7401e3e2d8225a3149162f243251385b3f968cd
Bresko <48460544+Bresko@users.noreply.github.com>
2021-01-12 19:24:07 +0100

    .

--- src/typing_biometrics/core.clj
+++ src/typing_biometrics/core.clj
@@ -6,49 +6,51 @@
   (:require [clojure.java.io :as io]))
 
 
-;; [[1 2][1 2][1]]
-;; koje slovo se najcesce brise
-;; koja je brzina kucanja 
-;; keystroke pattern recognition
-;; 
 (defn write [q]
    (with-open [w (io/writer "C:\\Users\\ITsec\\Desktop\\temp.txt" :append true)]
       (.write w (str q))))
 
-(defn write2 [action q millis username]
+(defn write2 [ a ms1 ms2 username]
    (with-open [w (io/writer (str "C:\\Users\\ITsec\\Desktop\\" username ".txt") :append true)]
-      (.write w (str action " " q " " millis "\n"))))
+      (.write w (str a " " ms1 " " ms2 " \n"))))
 
 
-(def key-pressed 0)
-(def key-released 0)
-(def key)
+(def key-pressed "null")
+(def key-released "null")
+(def old-k "null")
+(def old-key-released "null")
+(def old-new "null")
+
+
 
 (defn key-action [e action] 
   (let [k (.getRawCode e)]
     (str (print action " " k "  "))
     (println (System/currentTimeMillis))
-    
-    (println "old " key) ;;kada je key pressed onda se menja
-    (println "current " k)
-    (def key k)
-
 
 
     (if (= action "key-pressed")
-      (def key-pressed (System/currentTimeMillis))
-      (def key-released (System/currentTimeMillis)))
+      (do (def key-pressed (System/currentTimeMillis))
+          (def old-new [old-k k]))
+    (do
+      (def key-released (System/currentTimeMillis))
+      (write2  old-new (- key-released key-pressed) (- key-pressed old-key-released) "temp2")
+      (def old-new []))
+    
+    )
 
-    (println "aa " key-pressed)
-    (println "bb " key-released)
-    (println "cc " (- key-pressed key-released))
 
-    (if (= action "key-released")
-      (write2 action k (- key-released key-pressed) "temp2"))
-    ;;(write2 action k (System/currentTimeMillis) "temp2" )
+
+    (def old-k k)
+    (def old-key-released key-released)
+    
+    
     )
   )
-    ;;(write2 action k (System/currentTimeMillis) "temp2" )))
+
+
+
+
 
 (defn key-typed [e] ;;keylogger;;;;;
   (let [k (.getKeyChar e)]
@@ -57,29 +59,18 @@
 (defn myGlobalKeyListener []
   (reify
     NativeKeyListener    
-    ;;(def currentTime(System/currentTimeMillis))
     (nativeKeyPressed [this event] (key-action event "key-pressed")) ;;button pressed and time
     (nativeKeyReleased [this event] (key-action event "key-released")) ;;button released and time
     (nativeKeyTyped [this event] (key-typed event)))) ;; standard keylogger
 
 
 (defn mainMethod []
-  (println "asdadasdasda")
-      (GlobalScreen/registerNativeHook)
-      (.addNativeKeyListener (GlobalScreen/getInstance) (myGlobalKeyListener))
-    (println "keylogger started")
-  
-  ) 
+  (GlobalScreen/registerNativeHook)
+  (.addNativeKeyListener (GlobalScreen/getInstance) (myGlobalKeyListener))
+  (println "keylogger started")) 
 
 
 
 
 (defn -main [& args]
-
-  
-  (mainMethod)
-
-  
-  )
-
-
+  (mainMethod))
--- test/typing_biometrics/core_test.clj
+++ test/typing_biometrics/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [typing-biometrics.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;;
+
+
+
+
+
+
+
+
+
